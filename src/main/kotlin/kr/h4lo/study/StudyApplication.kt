36506d09c2bcd7bb254997c6d729d0d268d3c9e5
JaehunYoon <goodasd123@gmail.com>
2019-01-25 16:05:08 +0900

    Initial commit

---
+++ src/main/kotlin/kr/h4lo/study/StudyApplication.kt
@@ -0,0 +1,12 @@
+package kr.h4lo.study
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudyApplication
+
+fun main(args: Array<String>) {
+	runApplication<StudyApplication>(*args)
+}
+
