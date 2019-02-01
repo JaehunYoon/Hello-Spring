15a31de573d2e9e27ff2b7d4b59acccb53c58abc
JaehunYoon <goodasd123@gmail.com>
2019-02-01 17:34:07 +0900

    HomeController 기능 분리

--- src/main/kotlin/kr/h4lo/study/controller/HomeController.kt
+++ src/main/kotlin/kr/h4lo/study/controller/HomeController.kt
@@ -1,26 +1,12 @@
 package kr.h4lo.study.controller
 
-import kr.h4lo.study.model.Comment
-import kr.h4lo.study.model.Post
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/")
 class HomeController {
-
     @GetMapping("/")
     fun home(): String = "Home"
-
-    @GetMapping("posts")
-    fun getPosts(): String = "test"
-
-    @RequestMapping("posts/{id}", method = [RequestMethod.GET])
-    fun getPost(@PathVariable id: Int): String {
-        return "test.$id"
-    }
-
-    @RequestMapping("posts/{id}", method = [RequestMethod.POST])
-    fun post(@PathVariable id: Int): Post {
-        return Post(id, "test", "test", listOf(Comment("commenter", "content"), Comment("foo", "bar")))
-    }
 }
