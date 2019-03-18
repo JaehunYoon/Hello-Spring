47004d785675593b59ab732d61c4eb1cd3e5d37b
JaehunYoon <goodasd123@gmail.com>
2019-03-18 09:27:07 +0900

    Model에 Entity 어노테이션 적용

--- src/main/kotlin/kr/h4lo/study/model/Post.kt
+++ src/main/kotlin/kr/h4lo/study/model/Post.kt
@@ -1,7 +1,16 @@
 package kr.h4lo.study.model
 
-data class Post (val id: Int,
+import javax.persistence.*
+
+@Entity
+data class Post (@Id
+                 @GeneratedValue(strategy = GenerationType.AUTO)
+                 val id: Int,
+                 @Column(nullable = false)
                  val title: String,
+                 @Column(nullable = false)
                  val content: String,
+                 @Column(nullable = true)
                  val comment: List<Comment>?,
+                 @Column(nullable = false)
                  val createdAt: String)
