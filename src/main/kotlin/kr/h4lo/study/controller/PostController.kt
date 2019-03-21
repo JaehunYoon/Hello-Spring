e84a8ee455b57696d054e4a5d67d8bf8e0bfa778
JaehunYoon <goodasd123@gmail.com>
2019-03-21 10:12:02 +0900

    createdAt 자동 추가 구현

--- src/main/kotlin/kr/h4lo/study/controller/PostController.kt
+++ src/main/kotlin/kr/h4lo/study/controller/PostController.kt
@@ -1,6 +1,5 @@
 package kr.h4lo.study.controller
 
-import kr.h4lo.study.model.Comment
 import kr.h4lo.study.model.Post
 import kr.h4lo.study.repository.PostRepository
 import org.springframework.http.HttpStatus
@@ -23,6 +22,8 @@ class PostController (val postRepository: PostRepository) {
 
     @PostMapping
     fun newPost(@RequestBody post: Post): Post {
+        var target: Post = postRepository.findById(post.id).get()
+        target.createdAt = getCurrentTime()
         postRepository.save(post)
         return post
     }
@@ -41,31 +42,6 @@ class PostController (val postRepository: PostRepository) {
     fun deletePost(@PathVariable("postId") postId: Int) {
         postRepository.deleteById(postId)
     }
-//    @RequestMapping("posts/{id}", method = [RequestMethod.GET])
-//    fun getPost(@PathVariable id: Int): String {
-//        return "test.$id"
-//    }
-//
-//    @RequestMapping("posts/{id}", method = [RequestMethod.POST])
-//    fun createPost(@PathVariable id: Int): Post {
-//        val title = "test title"
-//        val content = "test content"
-//        val currentTime = getCurrentTime()
-//
-//        return Post(id, "test", "test", listOf(Comment("commenter", "content", currentTime), Comment("foo", "bar", currentTime)), currentTime)
-//    }
-//
-//    @RequestMapping("posts/{id}", method = [RequestMethod.PATCH])
-//    fun updatePost(@PathVariable id: Int): Post {
-//        val currentTime = getCurrentTime()
-//        return Post(id, "update", "update", listOf(Comment("update", "update", currentTime), Comment("update", "update", currentTime)), currentTime)
-//    }
-//
-//    @RequestMapping("posts/{id}", method = [RequestMethod.DELETE])
-//    fun deletePost(@PathVariable id: Int): String {
-//        return "delete"
-//    }
-
 
 //    Custom Function
     fun getCurrentTime() = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").withZone(ZoneOffset.UTC).format(Instant.now())!!
