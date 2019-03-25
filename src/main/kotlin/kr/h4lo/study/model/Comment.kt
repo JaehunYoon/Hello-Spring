c30eed94017a778e62f66f5895878955860881bc
JaehunYoon <goodasd123@gmail.com>
2019-03-25 13:28:00 +0900

    created_at 타임스탬프 자동 추가, CASCADE 적용

--- src/main/kotlin/kr/h4lo/study/model/Comment.kt
+++ src/main/kotlin/kr/h4lo/study/model/Comment.kt
@@ -1,10 +1,10 @@
 package kr.h4lo.study.model
 
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.OnDelete
+import org.hibernate.annotations.OnDeleteAction
+import java.util.*
 import javax.persistence.*
-
-//data class Comment (val commenter: String,
-//                    val content: String,
-//                    val createdAt: String)
 
 @Entity
 @Table(name = "comments")
@@ -15,8 +15,9 @@ data class Comment (@Id
                     val commenter: String,
                     @Column(nullable = false)
                     var content: String,
-                    @Column(nullable = true)
-                    val createdAt: String?,
+                    @CreationTimestamp
+                    val createdAt: Date?,
                     @ManyToOne(fetch = FetchType.LAZY, optional = false)
                     @JoinColumn(name = "post_id", nullable = false)
+                    @OnDelete(action = OnDeleteAction.CASCADE)
                     var post: Post?)
