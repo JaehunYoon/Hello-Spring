d1eb2ada27ff6494b26fac7b4c6306425081405d
JaehunYoon <goodasd123@gmail.com>
2019-04-03 15:10:10 +0900

    JPQL 적용, TypedQuery getComments 메소드 구현

--- src/main/kotlin/kr/h4lo/study/controller/CommentController.kt
+++ src/main/kotlin/kr/h4lo/study/controller/CommentController.kt
@@ -3,20 +3,26 @@ package kr.h4lo.study.controller
 import kr.h4lo.study.model.Comment
 import kr.h4lo.study.repository.CommentRepository
 import kr.h4lo.study.repository.PostRepository
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 import java.util.*
-import javax.persistence.EntityManagerFactory
-import javax.persistence.Persistence
+import javax.persistence.EntityManager
+import javax.persistence.TypedQuery
 
 @RestController
 @RequestMapping("/posts")
 class CommentController (val commentRepository: CommentRepository,
                          val postRepository: PostRepository) {
+    @Autowired
+    lateinit var entityManager: EntityManager
+
     @RequestMapping(path = ["/{postId}/comments"], method = [RequestMethod.GET])
-    fun getComments(@PathVariable("postId") postId: Int) {
-// 댓글을 불러오는 기능 구현
-        val emf: EntityManagerFactory = Persistence.createEntityManagerFactory("")
+    fun getComments(@PathVariable("postId") postId: Int): MutableList<Comment>? {
+        val getCommentsQuery = "SELECT C from Comment C WHERE C.post.id = :postId"
+        val query: TypedQuery<Comment> = entityManager.createQuery(getCommentsQuery, Comment::class.java)
+                query.setParameter("postId", postId)
+        return query.resultList
     }
 
     @RequestMapping(path = ["/{postId}/comments"], method = [RequestMethod.POST])
