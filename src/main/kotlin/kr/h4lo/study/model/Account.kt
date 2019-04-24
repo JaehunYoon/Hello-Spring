5817e31113923086e5f133ee7e9c79b80fd684fd
JaehunYoon <goodasd123@gmail.com>
2019-04-24 11:03:38 +0900

    Account 모델 수정

--- src/main/kotlin/kr/h4lo/study/model/Account.kt
+++ src/main/kotlin/kr/h4lo/study/model/Account.kt
@@ -1,14 +1,17 @@
 package kr.h4lo.study.model
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
+import javax.persistence.*
+import javax.validation.constraints.Size
 
 @Entity
+@Table(name = "accounts")
 data class Account (@Id
                     @GeneratedValue(strategy = GenerationType.AUTO)
                     var id: Int? = null,
+                    @Column(nullable = false, unique = true)
+                    @Size(min = 3, max = 255)
                     var userId : String,
+                    @Size(min = 8)
+                    @Column(nullable = false)
                     var password: String,
                     var nickname: String)
