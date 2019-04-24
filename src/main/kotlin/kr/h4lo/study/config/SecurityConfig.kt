43500531f87b9bffb556305410e2a78430f9166c
JaehunYoon <goodasd123@gmail.com>
2019-04-24 19:56:34 +0900

    Spring Security Configuration

--- src/main/kotlin/kr/h4lo/study/config/SecurityConfig.kt
+++ src/main/kotlin/kr/h4lo/study/config/SecurityConfig.kt
@@ -1,13 +1,22 @@
 package kr.h4lo.study.config
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
 @EnableWebSecurity
-class WebSecurityConfig : WebSecurityConfigurerAdapter() {
+class SecurityConfig : WebSecurityConfigurerAdapter() {
 
+    @Throws(Exception::class)
     override fun configure(web: WebSecurity?) {
         web!!.ignoring().antMatchers("/css/**", "/script/**", "image/**", "/fonts/**", "lib/**")
+    }
+
+    @Throws(Exception::class)
+    override fun configure(http: HttpSecurity) {
+        http.authorizeRequests()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/**").permitAll()
     }
 }
