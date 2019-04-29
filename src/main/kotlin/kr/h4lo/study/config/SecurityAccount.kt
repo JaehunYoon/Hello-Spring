2d27174516c06a83e5549ad3d7dc7f7b0f482d4d
JaehunYoon <goodasd123@gmail.com>
2019-04-29 22:18:24 +0900

    Spring Security SecurityAccount

--- src/main/kotlin/kr/h4lo/study/config/SecurityAccount.kt
+++ src/main/kotlin/kr/h4lo/study/config/SecurityAccount.kt
@@ -2,20 +2,22 @@ package kr.h4lo.study.config
 
 import kr.h4lo.study.model.Account
 import kr.h4lo.study.model.Role
-import org.springframework.boot.autoconfigure.security.SecurityProperties.User
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.User
 
-class SecurityAccount(account: Account) : User(account.id, account.password, makeGrantedAutority(account.roles)) {
+class SecurityAccount(account: Account) : User(account.userId, account.password, account.roles?.let { makeGrantedAuthority(it) }) {
+
     companion object {
-        private val ROLE_PREFIX = "ROLE_"
+        val ROLE_PREFIX = "ROLE_"
         private val serialVersionId = 1
 
-        private fun makeGrantedAutority(roles: List<Role>) : List<GrantedAuthority> {
-            val list = ArrayList()
+        private fun makeGrantedAuthority(roles: List<Role>): MutableList<GrantedAuthority> {
+            val list: MutableList<GrantedAuthority> = ArrayList()
             roles.forEach {
                 role -> list.add(SimpleGrantedAuthority(ROLE_PREFIX + role.roleName))
             }
+            return list
         }
     }
 }
