001c0b68cd16fb78acfdfdf0228c7d3811b13f0b
JaehunYoon <goodasd123@gmail.com>
2019-04-29 22:56:06 +0900

    Spring Security

--- src/main/kotlin/kr/h4lo/study/config/SecurityConfig.kt
+++ src/main/kotlin/kr/h4lo/study/config/SecurityConfig.kt
@@ -1,11 +1,15 @@
 package kr.h4lo.study.config
 
+import kr.h4lo.study.service.CustomUserDetailsService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 
 @EnableWebSecurity
 class SecurityConfig : WebSecurityConfigurerAdapter() {
@@ -20,12 +24,26 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
         http.authorizeRequests()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/**").permitAll()
+                .and().formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/")
+                .failureForwardUrl("/login")
+                .and()
+                .logout()
         http.csrf().disable()
+//        현 프로젝트는 API 기반이므로 form 을 연결할 수 없습니다.
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
     }
 
     @Autowired
     @Throws(Exception::class)
-    fun configureGlobal(auth: AuthenticationManagerBuilder) {
-
+    fun configureGlobal(auth: AuthenticationManagerBuilder,
+                        customUserDetailsService: CustomUserDetailsService) {
+        auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder())
     }
 }
--- src/main/kotlin/kr/h4lo/study/service/CustomUserDetailsService.kt
+++ src/main/kotlin/kr/h4lo/study/service/CustomUserDetailsService.kt
@@ -4,10 +4,13 @@ import kr.h4lo.study.repository.AccountRepository
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
 
-class CustomUserDetailsService (accountRepository: AccountRepository) : UserDetailsService {
+@Service
+class CustomUserDetailsService (val accountRepository: AccountRepository) : UserDetailsService {
     @Throws(UsernameNotFoundException::class)
-    override fun loadUserByUsername(username: String?): UserDetails? {
+    override fun loadUserByUsername(userId: String?): UserDetails? {
+//        return Optional.ofNullable(accountRepository.findByUserId(userId)).map()
         return null
     }
 }
